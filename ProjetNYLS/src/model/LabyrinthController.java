2f9ac4e281fc129ea658c3bfe2a12c5cd9961cb2
Lukyky414 <lucassschwab.e@gmail.com>
2018-11-12 13:30:39 +0000

    Mouvement (un peu) + naturel & bug map
    La map avait un bug, le teleporteur de la case 4.2 vers 2.4:
    la case 2.4 est un mur. J'ai change en 2.3
    
    C'est toujours pas ca, c'est trop rapide. Il faut changer totalement le deplacement.
    Peut etre un simple appui sur une touche ne fait deplacer que d'une case?

--- ProjetNYLS/src/model/LabyrinthController.java
+++ ProjetNYLS/src/model/LabyrinthController.java
@@ -8,6 +8,7 @@ import engine.GameController;
 
 /**
  * @author Horatiu Cirstea, Vincent Thomas
+ * Modifie par Lucas SCHWAB
  *
  * controleur de type KeyListener
  * 
@@ -15,25 +16,38 @@ import engine.GameController;
 public class LabyrinthController implements GameController {
 
 	/**
-	 * commande en cours
+	 * Bouton actuellement appuye
 	 */
 	private Cmd commandeEnCours;
+
+	/**
+	 * Dernier bouton appuye
+	 */
+	private Cmd lastCommande;
 	
 	/**
-	 * construction du controleur par defaut le controleur n'a pas de commande
+	 * Construction du controleur par defaut le controleur n'a pas de commande
 	 */
 	public LabyrinthController() {
 		this.commandeEnCours = Cmd.IDLE;
+		this.lastCommande = Cmd.IDLE;
 	}
 
 	/**
-	 * quand on demande les commandes, le controleur retourne la commande en
-	 * cours
+	 * Quand on demande les commandes, le controleur retourne la commande en
+	 * cours, ou la derniere commande apuye.
+	 * Ceci pour un deplacement plus naturel, un appuie tres court sur une
+	 * touche sera quand meme pris en compte
 	 * 
 	 * @return commande faite par le joueur
 	 */
 	public Cmd getCommand() {
-		return this.commandeEnCours;
+		Cmd res = this.commandeEnCours;
+		if(res == Cmd.IDLE){
+			res = this.lastCommande;
+			this.lastCommande = Cmd.IDLE;
+		}
+		return res;
 	}
 
 	@Override
@@ -47,21 +61,25 @@ public class LabyrinthController implements GameController {
 		case 'z':
 		case 'Z':
 			this.commandeEnCours = Cmd.UP;
+			this.lastCommande = Cmd.UP;
 			break;
 		// si on appuie sur 'q',commande joueur est gauche
 		case 'q':
 		case 'Q':
 			this.commandeEnCours = Cmd.LEFT;
+			this.lastCommande = Cmd.LEFT;
 			break;
 		// si on appuie sur 's',commande joueur est bas
 		case 's':
 		case 'S':
 			this.commandeEnCours = Cmd.DOWN;
+			this.lastCommande = Cmd.DOWN;
 			break;
 		// si on appuie sur 'd',commande joueur est droite
 		case 'd':
 		case 'D':
 			this.commandeEnCours = Cmd.RIGHT;
+			this.lastCommande = Cmd.RIGHT;
 			break;
 		}
 

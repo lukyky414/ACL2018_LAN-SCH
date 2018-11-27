c609a0e33602e11b015ae489c94b3b2f8ec8db20
Charlotte Lanuel <lanuel1u@fst-ats-303-l02.fst.site.univ-lorraine.fr>
2018-11-27 15:18:22 +0100

    debut fichier test

---
+++ ProjetNYLS/src/test/EntityTest.java
@@ -0,0 +1,44 @@
+package test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EntityTest {
+    @Test
+    public void getPos() throws Exception {
+    }
+
+    @Test
+    public void getHp() throws Exception {
+    }
+
+    @Test
+    public void getHpMax() throws Exception {
+    }
+
+    @Test
+    public void getAttack() throws Exception {
+    }
+
+    @Test
+    public void isDead() throws Exception {
+    }
+
+    @Test
+    public void diminuerHp() throws Exception {
+    }
+
+    @Test
+    public void attack() throws Exception {
+    }
+
+    @Test
+    public void canAttack() throws Exception {
+    }
+
+    @Test
+    public void resetCooldown() throws Exception {
+    }
+
+}
